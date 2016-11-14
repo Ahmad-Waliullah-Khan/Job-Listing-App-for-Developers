88bb39f6420b60c55e9a12464ad37c63d4bb6b43
oli18 <oli.de.psycho.gamer@gmail.com>
2016-11-14 19:17:48 +0530

    Added validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,19 @@ class ContactsController < ApplicationController
 	end
 
 	def create
+		@contact = Contact.new(contact_params)
+		
+		if @contact.save
+			flash[:success] = 'Message sent'
+			redirect_to new_contact_path, notice: "Message sent."
+		else
+			flash[:danger] = 'Error occured, message has not been sent'
+			redirect_to new_contact_path, notice: "Error."
+		end
 	end
 
+	private
+		def contact_params
+		params.require(:contact).permit(:name, :email, :comments)
+		end
 end
